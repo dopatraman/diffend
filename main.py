5d0f5b3ff818675721d6218acd01548b5107b7a9
Prakash Venkatraman <venkatraman.prakash@gmail.com>
2024-10-22 17:38:27 -0700

    Initial commit

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
+import json
 import openai
+import os
+import requests
 
 client = openai.OpenAI()
 
+def check_if_branch_exists(branch_name):
+    token = os.environ["GITHUB_API_KEY"]
+    if not token:
+        raise Exception("You need a Github API Key")
+    url = "/repos/dopatraman/diffend/branches/{branch_name}"
+    headers = {
+        'Authorization': 'token {token}'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status == 200:
+        return True
+    return False
+
+def get_diff(base, head):
+    token = os.environ["GITHUB_API_KEY"]
+    if not token:
+        raise Exception("You need a Github API Key")
+    url = f"https://api.github.com/repos/dopatraman/diffend/compare/{base}...{head}"
+    headers = {
+        "Authorization": f"token {token}"
+    }
+    response = requests.get(url, headers=headers)
+    if response.status == 200:
+        return True
+    return False
+
 # Find a way to generate these tool descriptions
 tools = [
-    {
-        "type": "function",
-        "function": {
-            "name": "check_if_branch_exists",
-            "description": "checks whether a branch exists",
-            "parameters": {
-                "type": "object",
-                "properties": {"branch_name": {"type": "string"}},
-                "required": ["branch_name"],
-            },
-        },
-    },
     {
         "type": "function",
         "function": {
@@ -30,7 +47,14 @@ tools = [
     },
 ]
 
+def run_after_check_branch_exists():
+    pass
+
 def run_conversation():
+    # Needs to be generated
+    tool_lookup = {
+        "get_diff": get_diff
+    }
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=[
@@ -44,6 +68,15 @@ def run_conversation():
     )
 
     message = response.choices[0].message
+
+    if message.content is None and len(message.tool_calls) > 0:
+        for tool_call in message.tool_calls:
+            if tool_call.function.name == "get_diff":
+                arguments = json.loads(tool_call.function.arguments)
+                base = arguments["base"]
+                head = arguments["head"]
+                diff = tool_lookup["get_diff"](base, head)
+        
 
     print(message.model_dump_json(indent=2))
 
